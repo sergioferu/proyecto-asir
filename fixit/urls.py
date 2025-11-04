9984106041c56b7ebc26db763bab07979f31b991
sergioferu <sergio.ferreira.ruiz.alu@iesfernandoaguilar.es>
2025-11-04 22:03:12 +0100

    Add files via upload

--- fixit/urls.py
+++ fixit/urls.py
@@ -1,8 +1,27 @@
 from django.urls import path
-from fixit.views import Incidencias_View, PerfilUsuario_View, Tecnico_View
+from fixit.views import (
+    Incidencias_View, PerfilUsuario_View, Tecnico_View,
+    IncidenciaCreateView, IncidenciaUpdateView, IncidenciaDeleteView,
+    PerfilUsuarioCreateView, PerfilUsuarioUpdateView, PerfilUsuarioDeleteView,
+    TecnicoCreateView, TecnicoUpdateView, TecnicoDeleteView,
+)
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("incidencias/", Incidencias_View.as_view()),
-    path("perfil_usuario/", PerfilUsuario_View.as_view()),
-    path("perfil_tecnico/", Tecnico_View.as_view()),
+    path("", TemplateView.as_view(template_name="fixit/home.html"), name="home"),
+
+    path("incidencias/", Incidencias_View.as_view(), name="incidencias_list"),
+    path("incidencias/create/", IncidenciaCreateView.as_view(), name="incidencia_create"),
+    path("incidencias/<int:pk>/update/", IncidenciaUpdateView.as_view(), name="incidencia_update"),
+    path("incidencias/<int:pk>/delete/", IncidenciaDeleteView.as_view(), name="incidencia_delete"),
+
+    path("perfil_usuario/", PerfilUsuario_View.as_view(), name="perfilusuario_list"),
+    path("perfil_usuario/create/", PerfilUsuarioCreateView.as_view(), name="perfilusuario_create"),
+    path("perfil_usuario/<int:pk>/update/", PerfilUsuarioUpdateView.as_view(), name="perfilusuario_update"),
+    path("perfil_usuario/<int:pk>/delete/", PerfilUsuarioDeleteView.as_view(), name="perfilusuario_delete"),
+
+    path("perfil_tecnico/", Tecnico_View.as_view(), name="tecnico_list"),
+    path("perfil_tecnico/create/", TecnicoCreateView.as_view(), name="tecnico_create"),
+    path("perfil_tecnico/<int:pk>/update/", TecnicoUpdateView.as_view(), name="tecnico_update"),
+    path("perfil_tecnico/<int:pk>/delete/", TecnicoDeleteView.as_view(), name="tecnico_delete"),
 ]
--- fixit/views.py
+++ fixit/views.py
@@ -1,15 +1,74 @@
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from fixit.models import Incidencia, PerfilUsuario, Tecnico
 
 
+# Vistas del modelo Incidencia
 class Incidencias_View(ListView):
     model = Incidencia
     context_object_name = "Vista del contenido de la tabla Incidencias"
+    template_name = "fixit/incidencia/list.html"
 
+class IncidenciaCreateView(CreateView):
+    model = Incidencia
+    fields = '__all__'
+    template_name = "fixit/incidencia/form.html"
+    success_url = reverse_lazy('incidencias_list')
+
+class IncidenciaUpdateView(UpdateView):
+    model = Incidencia
+    fields = '__all__'
+    template_name = "fixit/incidencia/form.html"
+    success_url = reverse_lazy('incidencias_list')
+
+class IncidenciaDeleteView(DeleteView):
+    model = Incidencia
+    template_name = "fixit/incidencia/confirm_delete.html"
+    success_url = reverse_lazy('incidencias_list')
+
+# Vistas del modelo PerfilUsuario
 class PerfilUsuario_View(ListView):
     model = PerfilUsuario
     context_object_name = "Vista del contenido de la tabla PerfilUsuario"
+    template_name = "fixit/perfilusuario/list.html"
 
+class PerfilUsuarioCreateView(CreateView):
+    model = PerfilUsuario
+    fields = '__all__'
+    template_name = "fixit/perfilusuario/form.html"
+    success_url = reverse_lazy('perfilusuario_list')
+
+class PerfilUsuarioUpdateView(UpdateView):
+    model = PerfilUsuario
+    fields = '__all__'
+    template_name = "fixit/perfilusuario/form.html"
+    success_url = reverse_lazy('perfilusuario_list')
+
+class PerfilUsuarioDeleteView(DeleteView):
+    model = PerfilUsuario
+    template_name = "fixit/perfilusuario/confirm_delete.html"
+    success_url = reverse_lazy('perfilusuario_list')
+
+# Vistas del modelo Tecnico
 class Tecnico_View(ListView):
     model = Tecnico
     context_object_name = "Vista del contenido de la tabla Tecnico"
+    template_name = "fixit/tecnico/list.html"
+
+class TecnicoCreateView(CreateView):
+    model = Tecnico
+    fields = '__all__'
+    template_name = "fixit/tecnico/form.html"
+    success_url = reverse_lazy('tecnico_list')
+
+class TecnicoUpdateView(UpdateView):
+    model = Tecnico
+    fields = '__all__'
+    template_name = "fixit/tecnico/form.html"
+    success_url = reverse_lazy('tecnico_list')
+
+class TecnicoDeleteView(DeleteView):
+    model = Tecnico
+    template_name = "fixit/tecnico/confirm_delete.html"
+    success_url = reverse_lazy('tecnico_list')
